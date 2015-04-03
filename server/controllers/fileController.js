8a358c68fd38dd9fb2fb9dc69e6ea07d5df48ef0
avnersch <avnerz@gmail.com>
2015-04-03 14:07:20 +0300

    remove node_modules

--- server/controllers/fileController.js
+++ server/controllers/fileController.js
@@ -7,5 +7,9 @@
 exports.upload = function(req, res) {
 
     console.log('POST - /file/upload');
+    // access req.files.file thanks to the multiparty middleware
+    var file = req.files.file;
+    console.log(file.name);
+    console.log(file.type);
 };
 
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,7 +1,10 @@
 var app = require('express');
 var router = app.Router();
+var multiparty = require('connect-multiparty');
+var multipartyMiddleware = multiparty();
+var fileController = require('../controllers/fileController.js');
 
-router.post('/file/upload', fileController.upload);
+router.post('/file/upload', multipartyMiddleware, fileController.upload);
 router.get('*', function (req, res) {
   res.render('index.html', { user: req.user ? req.user : null });
 });
