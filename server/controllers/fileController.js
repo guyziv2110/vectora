00fcd3cf01814b482ab1e559dc9a601e857a98ca
avnersch <avnerz@gmail.com>
2015-04-03 15:59:25 +0300

    added fileController behavior.
    uploaded files currently written to /uploads

--- server/controllers/fileController.js
+++ server/controllers/fileController.js
@@ -1,4 +1,6 @@
 // fileController
+var fs = require('fs');
+var path = require('path');
 
 /**
  * @param {Object} req HTTP request object.
@@ -9,7 +11,17 @@ exports.upload = function(req, res) {
     console.log('POST - /file/upload');
     // access req.files.file thanks to the multiparty middleware
     var file = req.files.file;
-    console.log(file.name);
-    console.log(file.type);
+    var fileName = file.name;
+    var appDir = path.dirname(require.main.filename);
+    var tmpPathFile = file.path;
+    var uploadPath = path.normalize(appDir + '/uploads/');
+    var uploadPathFile = uploadPath + fileName;
+
+    fs.rename(tmpPathFile, uploadPathFile, function(err) {
+      if (err) {
+        return console.log('file upload failure. ' + err);
+      }
+      console.log('file upload success. path: ' + uploadPathFile);
+    });
 };
 
