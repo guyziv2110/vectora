a088eb8a482e8ab62d4c28cdee1e0f26c4138c2f
avnersch <avnerz@gmail.com>
2015-04-08 15:48:01 +0300

    change passportjs config to be relative instead of absolute

--- server/auth/auth-config.js
+++ server/auth/auth-config.js
@@ -3,13 +3,11 @@ module.exports = {
   'facebookAuth' : {
     'clientID'      : '1654108154819023', 
     'clientSecret'  : 'c6b2e201582310b448ef45b610942067', 
-    'callbackURL'   : 'http://localhost:3000/auth/facebook/callback'
   },
 
   'googleAuth' : {
     'clientID'      : '1008550286704-s7dgq33cdi460fj972as6mpia1odq2ul.apps.googleusercontent.com',
     'clientSecret'  : '06s5Zp2IqpN-4LbURLt2FBRl',
-    'callbackURL'   : 'http://localhost:3000/auth/google/callback'
   }
 
 };
